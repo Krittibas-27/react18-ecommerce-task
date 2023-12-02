cc958aa2847ada855e0a5245dc1ec4e030524025
Krittibas-27 <lets1954@gmail.com>
2023-12-02 23:57:58 +0530

    Add to cart done

--- src/PlatziProductList.js
+++ src/PlatziProductList.js
@@ -13,19 +13,20 @@ import {
 import { RootProductServer } from "./config/RootApi";
 import SpinnerComponent from "./components/SpinnerComponent";
 import { useParams } from "react-router-dom";
-import { FiShoppingCart,FiMinus } from "react-icons/fi";
+import { FiShoppingCart, FiMinus } from "react-icons/fi";
 import { FaPlus } from "react-icons/fa6";
 
 const PlatziProductList = () => {
-  const {catid} = useParams();
+  const { catid } = useParams();
   //console.log('catid-', catid)
   const [resCategoryProdicts, setResCategoryProdicts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [query, setQuery] = useState("");
   const [viewModal, setViewModal] = useState(false);
-  const [singleProduct, setSingleProduct]=useState()
-  const [quantityValue, setQuantityValue]=useState(1)
+  const [singleProduct, setSingleProduct] = useState();
+  const [quantityValue, setQuantityValue] = useState(1);
+  const [allSelectedProduct, setAllSelectedProduct] = useState([]);
 
   const getAllCatProducts = () => {
     setIsLoading(true);
@@ -52,22 +53,28 @@ const PlatziProductList = () => {
     setQuery("");
   };
   const selectedProduct = (data) => {
-    console.log("data-", data);
-    setViewModal(true)
-    setSingleProduct(data)
+    //console.log("data-", data);
+    setViewModal(true);
+    setSingleProduct(data);
   };
-  const quantityPlus=()=>{
-    setQuantityValue(quantityValue + 1)
-  }
-  const quantityMinus=()=>{
-    if(quantityValue!==1){
-      setQuantityValue(quantityValue - 1)  
+  const quantityPlus = () => {
+    setQuantityValue(quantityValue + 1);
+  };
+  const quantityMinus = () => {
+    if (quantityValue !== 1) {
+      setQuantityValue(quantityValue - 1);
     }
-  }
+  };
+  const addToCartProduct = (item) => {
+     //if (allSelectedProduct.indexOf(item)) return;
+     if (allSelectedProduct.indexOf(item) !== -1) return
+     setAllSelectedProduct([...allSelectedProduct, item]);
+    console.log('click')
+  };
   useEffect(() => {
     getAllCatProducts();
   }, []);
-  //console.log('singleProduct-',singleProduct)
+  console.log("allSelectedProduct-", allSelectedProduct);
   return (
     <div className="my-4">
       <Container>
@@ -102,13 +109,21 @@ const PlatziProductList = () => {
                     <Card.Img
                       className="card_img2"
                       variant="top"
-                      src={item.images[0]} 
-                      onClick={()=>selectedProduct(item)}  
+                      src={item.images[0]}
+                      onClick={() => selectedProduct(item)}
                     />
                     <Card.Body>
                       <Card.Title className="h6">{item.title}</Card.Title>
-                      <Card.Subtitle className="mb-2 text-muted">Price - ${item.price}</Card.Subtitle>
-                      <Button variant="primary">
+                      <Card.Subtitle className="mb-2 text-muted">
+                        Price - ${item.price}
+                      </Card.Subtitle>
+                      <Button
+                        variant="primary"
+                        onClick={() => addToCartProduct(item)}
+                        disabled={allSelectedProduct.some(
+                          (ele) => ele.id === item.id
+                        )}
+                      >
                         <FiShoppingCart /> Add to Cart
                       </Button>
                     </Card.Body>
@@ -120,38 +135,49 @@ const PlatziProductList = () => {
         </Row>
       </Container>
 
-      <Modal centered size="lg" show={viewModal} onHide={()=>setViewModal(false)}>
+      <Modal
+        centered
+        size="lg"
+        show={viewModal}
+        onHide={() => setViewModal(false)}
+      >
         <Modal.Header closeButton>
-        <Modal.Title>{singleProduct?.title}</Modal.Title>
+          <Modal.Title>{singleProduct?.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <Row className="align-items-center">
-                <Col md={6}>
-                <Image src={singleProduct?.images[0]} thumbnail  />
-                </Col>
-                <Col md={6}>
-                    <small>{singleProduct?.category?.name}</small>
-                    <h5 className="text-dark">{singleProduct?.title}</h5>
-                    {/* <p>{singleProduct.description}</p> */}
-                    <span>Price - <strong>${singleProduct?.price}</strong></span>
-                    <div className="d-flex mb-3 align-items-center">
-                        <span>Quantity  </span>
-                        <Button variant="default" onClick={()=>quantityMinus()}><FiMinus/></Button>
-                        <div className="quantity_box">{quantityValue}</div>
-                        <Button variant="default" onClick={()=>quantityPlus()}><FaPlus /></Button>
-                    </div>
-                    <Button variant="primary">
-                        <FiShoppingCart /> Add to Cart
-                      </Button>
-                </Col>
-            </Row>
+          <Row className="align-items-center">
+            <Col md={6}>
+              <Image src={singleProduct?.images[0]} thumbnail />
+            </Col>
+            <Col md={6}>
+              <small>{singleProduct?.category?.name}</small>
+              <h5 className="text-dark">{singleProduct?.title}</h5>
+              {/* <p>{singleProduct.description}</p> */}
+              <span>
+                Price - <strong>${singleProduct?.price}</strong>
+              </span>
+              <div className="d-flex mb-3 align-items-center">
+                <span>Quantity </span>
+                <Button variant="default" onClick={() => quantityMinus()}>
+                  <FiMinus />
+                </Button>
+                <div className="quantity_box">{quantityValue}</div>
+                <Button variant="default" onClick={() => quantityPlus()}>
+                  <FaPlus />
+                </Button>
+              </div>
+              <Button variant="primary" onClick={() => addToCartProduct(singleProduct)}
+                        disabled={allSelectedProduct.some(
+                          (ele) => ele.id === singleProduct.id
+                        )}>
+                <FiShoppingCart /> Add to Cart
+              </Button>
+            </Col>
+          </Row>
         </Modal.Body>
-        </Modal>
+      </Modal>
     </div>
-
-
-  )
-  
+  );
 };
 
 export default PlatziProductList;
